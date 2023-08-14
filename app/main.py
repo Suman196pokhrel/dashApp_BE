f4f57942a2c66a1cef83ecc3c5ca88663407ceb4
Suman Pokhrel <spokhrel196@gmail.com>
2023-08-14 19:02:46 +0545

    changed allowed oririns

--- app/main.py
+++ app/main.py
@@ -8,10 +8,10 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # allowed origins
-frontEnd = 'http://64.227.166.179:3000'
+frontEnd = ['http://64.227.166.179:300/',"http://localhost:3000"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[frontEnd],
+    allow_origins=frontEnd,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
